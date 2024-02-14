d9ac6108e84335894f5127f30bc82c00c284fc82
Verth <vertisixs@gmail.com>
2024-02-14 13:49:05 +0100

    Table pagination

--- script.js
+++ script.js
@@ -1,10 +1,91 @@
+let orderByV = 'marketCap';
 let orderDirectionV = false;
-let limitV = 50;
+let limitV = 100;
+let offsetV = 0;
+let activePage = 0;
+let lastPage;
 
-async function getAllCoins(orderBy = 'marketCap', orderDirection = false, limit = 50) {
+async function init() {
+  getAllCoins();
+  getLastInRank();
+}
+
+async function getLastInRank() {
+  const url = `https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=asc&limit=1&offset=0`;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '92cafd2032msh75252a91a66893ap1128bdjsn75fcdc8c70ba',
+      'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    displayPagination(result.data.coins);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function displayPagination([lastCoin]) {
+  // console.log(lastCoin.rank);
+  // ilość stron = lastCoin.rank / limitV
+  const pagesCount = lastCoin.rank / limitV;
+  console.log(lastCoin.rank);
+
+  const mainDivElement = document.getElementById('table-navigation');
+
+  const paginationElements = Array.from({ length: lastCoin.rank % limitV === 0 ? pagesCount : pagesCount + 1 }, (_, index) => index);
+  lastPage = paginationElements.length - 1;
+  console.log(lastPage);
+
+  mainDivElement.innerHTML = `
+        <div class="pagination">
+          <div class="pagination-elements">
+            <a href="#" id='back'>&laquo;</a>
+            ${paginationElements.map((page) => `<a id=p-${page} ${activePage === page ? 'class=active' : ''} href="#" onclick="goPagination(${page})">${page + 1}</a>`).join('')}
+            <a href="#" id='next'>&raquo;</a>
+          </div>
+        </div>
+    `;
+}
+
+function goPagination(pageCount) {
+  offsetV = pageCount * limitV;
+
+  const curActivePag = document.getElementById(`p-${activePage}`);
+  curActivePag.classList.remove('active');
+
+  const newActivePag = document.getElementById(`p-${pageCount}`);
+  newActivePag.classList.add('active');
+
+  activePage = pageCount;
+
+  const backPag = document.getElementById('back');
+  const nextPag = document.getElementById('next');
+
+  if (activePage !== 0) {
+    backPag.setAttribute('onclick', `goPagination(${pageCount - 1})`);
+  } else {
+    backPag.removeAttribute('onclick');
+  }
+  if (activePage !== lastPage) {
+    nextPag.setAttribute('onclick', `goPagination(${pageCount + 1})`);
+  } else {
+    nextPag.removeAttribute('onclick');
+  }
+
+  getAllCoins(orderByV, orderDirectionV, limitV, offsetV);
+}
+
+async function getAllCoins(orderBy = 'marketCap', orderDirection = false, limit = 50, offset = 0) {
   const url = `https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=${orderBy}&orderDirection=${
     orderDirection ? 'asc' : 'desc'
-  }&limit=${limit}&offset=0`;
+  }&limit=${limit}&offset=${offset}`;
+
   const options = {
     method: 'GET',
     headers: {
@@ -24,28 +105,31 @@ async function getAllCoins(orderBy = 'marketCap', orderDirection = false, limit 
   document.querySelector('.rank-button').addEventListener('click', function (e) {
     e.preventDefault();
     orderDirectionV = !orderDirectionV;
-    getAllCoins('marketCap', orderDirectionV);
+    orderByV = 'marketCap';
+    getAllCoins(orderByV, orderDirectionV, limitV, offsetV);
   });
   document.querySelector('.price-button').addEventListener('click', function (e) {
     e.preventDefault();
     orderDirectionV = !orderDirectionV;
-    getAllCoins('price', orderDirectionV);
+    orderByV = 'price';
+    getAllCoins(orderByV, orderDirectionV, limitV, offsetV);
   });
   document.querySelector('.change-button').addEventListener('click', function (e) {
     e.preventDefault();
     orderDirectionV = !orderDirectionV;
-    getAllCoins('change', orderDirectionV);
+    orderByV = 'change';
+    getAllCoins(orderByV, orderDirectionV, limitV, offsetV);
   });
 }
 
-getAllCoins();
+init();
 // const timestamp = 1707769816 * 1000;
 // console.log(new Date(timestamp));
 
 function displayData(coins) {
   //const coins = result.data.coins;
 
-  console.log(coins);
+  // console.log(coins);
 
   const mainDivElement = document.getElementById('main');
 
@@ -117,5 +201,5 @@ function changeLimit() {}
 - [] prompt -> allows setting the limit of coins shown in the list when clicked on a button (maybe on the navbar)
 - [] alert -> when updating records ("records updated successfully") or just an alert button for triggering alerts or alert for confirmation on database actions
 - [?] html5 -> navbar and footnote
-- [] pagination -> by using offset and division by the limit of rows in table (for automatic scalable pagination - check the rank number of the last item in table)
+- [x] pagination -> by using offset and division by the limit of rows in table (for automatic scalable pagination - check the rank number of the last item in table)
 */
